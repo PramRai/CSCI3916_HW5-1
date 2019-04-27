9f13d11ccd3fa7c5893f20e40a7a18f41075c7c7
Oleksiy Pobyeda <oleksiypobyeda@gmail.com>
2019-04-27 00:24:41 -0600

    movie static page fix

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -56,7 +56,7 @@ export function fetchMovies(){
 export function fetchMovie(movieId){
     const env = runtimeEnv();
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/movie/${movieId}?reviews=true`, {
+        return fetch(`${env.REACT_APP_API_URL}/movie/:${movieId}?reviews=true`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
