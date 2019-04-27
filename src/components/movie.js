58517017d031de9463b0eccdd61cfdae565570bf
Oleksiy Pobyeda <oleksiypobyeda@gmail.com>
2019-04-27 01:37:10 -0600

    display review place for user

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,18 +1,72 @@
 import React, { Component }  from 'react';
 import {connect} from "react-redux";
-import { Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Glyphicon, Panel, ListGroup, ListGroupItem, Form, FormGroup, Col, ControlLabel, FormControl, Button,} from 'react-bootstrap'
 import { Image } from 'react-bootstrap'
 import { withRouter } from "react-router-dom";
 import {fetchMovie} from "../actions/movieActions";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
 
 //support routing by creating a new component
 
 class Movie extends Component {
 
+    constructor(props) {
+        super(props);
+        this.updateDetails = this.updateDetails.bind(this);
+        this.reviewSub = this.reviewSub.bind(this);
+
+        this.state = {
+            details:{
+                review: '',
+                rating: 0
+            }
+        };
+    }
+
     componentDidMount() {
         const {dispatch} = this.props;
         if (this.props.selectedMovie == null)
             dispatch(fetchMovie(this.props.movieId));
+    }
+
+    updateDetails(event){
+        let updateDetails = Object.assign({}, this.state.details);
+
+        updateDetails[event.target.id] = event.target.value;
+        this.setState({
+            details: updateDetails
+        });
+    }
+
+    reviewSub() {
+        const env = runtimeEnv();
+
+        var json = {
+            Review: this.state.details.review,
+            Rating: this.state.details.rating,
+            Movie_ID: this.props.movieId
+        };
+
+        return fetch(`${env.REACT_APP_API_URL}/review`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify(json),
+            mode: 'cors'})
+            .then( (response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then( (res) => {
+                window.location.reload();
+            })
+            .catch( (e) => console.log(e) );
+
     }
 
     render() {
@@ -50,8 +104,39 @@ class Movie extends Component {
                 </Panel>
             );
         };
+
         return (
-            <DetailInfo currentMovie={this.props.selectedMovie} />
+            <div>
+                <DetailInfo currentMovie={this.props.selectedMovie} />
+                <Form horizontal>
+                    <FormGroup controlId="review">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Review
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl onChange={this.updateDetails} value={this.state.details.review} type="text" placeholder="type review" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup controlId="rating">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Rating
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl onChange={this.updateDetails}
+                                         value={this.state.details.rating}
+                                         type="number" min="1" max="5" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button onClick={this.reviewSub}>Submit</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+
+            </div>
         );
     }
 }
